f524894f0aa2f9409726fbfaaaccae762990c235
Frederik Dudzik <frederik.dudzik@gmail.com>
2015-11-16 23:10:52 +0100

    add emiting multiple messages from same client obj

--- src/client.js
+++ src/client.js
@@ -4,14 +4,20 @@ import Middleware  from 'frp-middleware'
 class Client extends Middleware {
   constructor() {
     super()
-    this.socket = new net.Socket();
-    this.args   = arguments
+    this.args = arguments
   }
 
   emit(message, fn = x => x) {
     const middleware = super.emit()
-    this.socket.connect(...this.args, () => this.socket.write(middleware(message)))
-    this.socket.on('data',  data => fn(data.toString()))
+    var socket = new net.Socket()
+
+    socket.connect(...this.args, () => {
+      socket.write(middleware(message))
+    })
+
+    socket.on('data', data => {
+      fn(data.toString())
+    })
   }
 }
 
--- test/client-test.js
+++ test/client-test.js
@@ -3,7 +3,7 @@ import net    from 'net'
 import Client from '../src/client'
 
 describe('Client', () => {
-  it('sends messenges', done => {
+  it('sends messenge', done => {
     var server = net.createServer(socket => {
       socket.on('data', data =>{
         assert(data == 'foo')
@@ -17,14 +17,33 @@ describe('Client', () => {
     client.emit('foo')
   })
 
+  it('sends multiple messenges', done => {
+    var counter = 0
+    var server  = net.createServer(socket => {
+      socket.on('data', data => {
+        assert(data == 'foo')
+        if(counter == 2)
+          done()
+        counter++
+      })
+    })
+
+    server.listen(1338, '127.0.0.1')
+
+    var client = new Client(1338, '127.0.0.1')
+    client.emit('foo')
+    client.emit('foo')
+    client.emit('foo')
+  })
+
   it('receives respond', done => {
     var server = net.createServer(socket => {
       socket.on('data', data => socket.write('bar'))
     })
 
-    server.listen(1338)
+    server.listen(1339)
 
-    new Client(1338).emit('foo', data => {
+    new Client(1339).emit('foo', data => {
       data = data.toString()
       assert(data == 'bar')
       done()
